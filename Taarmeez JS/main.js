5b3ecc6df1f2e6efd0a45bfbd061e8eaf618973a
Eng.Tala <talabts33@gmail.com>
2026-03-11 22:50:13 +0200

    first commit

---
+++ Taarmeez JS/main.js
@@ -0,0 +1,94 @@
+//Varibles 
+//Var
+// var x=" Tala";
+// x = 50
+// console.log(x)
+// console.log("x")
+//not is not defined
+// console.log(y)//error is not define (غير معرف ) 
+// const t=-200+5;
+// const r=10/2
+// console.log(r)
+// console.log(5+5)
+// console.log(t)
+// let a =10
+// a++//a=a+1 && a+=1
+// console.log(a)
+//Varibles challenge 1
+// var num1=10;
+// var num2=4;
+// console.log("First number is :"+num1)
+// console.log("Second number is :"+num2)
+// console.log("Sum is :"+(num1+num2))
+// console.log("Sub is :"+(num1-num2))
+// console.log("Div is :"+(num1/num2))
+// console.log("Mod is :"+(num1%num2))
+// let ht=document.querySelector('h3')
+// ht.innerText=num2
+// ht.innerHTML=9num1
+//////////////////////////////////////////
+//Data Type in JS(typeof)=>KeyWords
+// console.log(typeof "tala")
+// console.log(typeof(2))
+// console.log(typeof [])//array==obj in js
+//////////////////////////////////
+//function in String 
+// let title="hello world"
+//substring function(بتقص النص بحدد اله نقطة البداية ونقطة النهاية )
+//الترتيب والعد يبدأ من صفر لحد ال i-1
+// console.log(title.substring (0,5))
+/////////////////////////////////////////
+// length (خاصية هاي ف ما بستخدم معها اقواس )
+// console.log(title.length)
+////////////////////////////////////////
+//toUpperCase 
+// console.log(title.toUpperCase())
+// // toLowerCase
+// console.log(title.toLowerCase())
+// //Replace
+// console.log(title.replace("l","r"))
+// //charAt
+// console.log( title.charAt(1))// =>e
+// console.log(title[1])// === charAt 
+//////////////////////////////////
+// String challenge 
+// let V="Hello World"
+// console.log(V.toUpperCase())
+// console.log(V.replace("o","Y"))
+// //reverse
+// console.log(V.split("").reverse("").join(""))
+// /بحتاج اني احولها لمصفوفة عشان اعكس م بقدر اعكس وهو String
+////////////////////////////////////////////////////
+//بدي اخد المحتوى الي بال h
+// let h1=document.querySelector("h1").innerHTML
+// console.log(h1)
+// console.log(h1.toUpperCase())
+// console.log(h1.replace('o','y'))
+////////////////////////////////////
+let btn =document.querySelector('button')
+btn.addEventListener("click",()=>{
+    let inp = document.querySelector('input').value
+    console.log(inp)
+    //هان انا اخدت قيمة متغيرة وبدي اسوي عليها نفس العمليات الي فوق 
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
